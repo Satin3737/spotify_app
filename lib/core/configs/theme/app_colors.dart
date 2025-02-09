66282a527f85cdc1923f6acc21e65fddb25ab39e
satin <satin3737@gmail.com>
2025-02-09 15:32:15 +0200

    Added basic theme setup

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF42C83C);
+
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const blue = Color(0xFF288CE9);
+  static const grey = Color(0xFF797979);
+
+  static const lightBg = Color(0xFFF2F2F2);
+  static const lightText = Color(0xFF383838);
+
+  static const darkBg = Color(0xFF1C1B1B);
+  static const darkText = Color(0xFFE1E1E1);
+}
---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_app/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  static final _elevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: AppColors.primary,
+      textStyle: TextStyle(
+        color: AppColors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(32),
+      ),
+    ),
+  );
+
+  static final _baseTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+    elevatedButtonTheme: _elevatedButtonTheme,
+    fontFamily: 'Satoshi',
+  );
+
+  static final lightTheme = _baseTheme.copyWith(
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppColors.lightBg,
+  );
+
+  static final darkTheme = _baseTheme.copyWith(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.darkBg,
+  );
+}

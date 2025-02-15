3c4d8c1a7cdfa4aca9b7c9089409f5a12780c3aa
satin <satin3737@gmail.com>
2025-02-15 13:45:19 +0200

    Added all other images and svg to assets classes

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -4,4 +4,6 @@ class AppImages {
 
   static const String introBg = '${_basePath}intro_bg$_format';
   static const String chooseModeBg = '${_basePath}choose_mode_bg$_format';
+  static const String authBg = '${_basePath}auth_bg$_format';
+  static const String homeArtist = '${_basePath}home_artist$_format';
 }
--- lib/core/configs/assets/app_svg.dart
+++ lib/core/configs/assets/app_svg.dart
@@ -5,4 +5,7 @@ class AppSvg {
   static const String spotifyLogo = '${_basePath}spotify_logo$_format';
   static const String sunIcon = '${_basePath}sun$_format';
   static const String moonIcon = '${_basePath}moon$_format';
+  static const String topPattern = '${_basePath}top_pattern$_format';
+  static const String bottomPattern = '${_basePath}bottom_pattern$_format';
+  static const String homeTopCard = '${_basePath}home_top_card$_format';
 }

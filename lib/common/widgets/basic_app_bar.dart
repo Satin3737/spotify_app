be735d71c7291bb92cb970ea6e39038c58ec3518
satin <satin3737@gmail.com>
2025-04-06 11:24:01 +0300

    Added hide back param to the appbar widget

--- lib/common/widgets/basic_app_bar.dart
+++ lib/common/widgets/basic_app_bar.dart
@@ -3,9 +3,14 @@ import 'package:spotify_app/common/helpers/is_dark_mode.dart';
 import 'package:spotify_app/core/configs/theme/app_colors.dart';
 
 class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const BasicAppBar({super.key, this.title});
-
   final Widget? title;
+  final bool? hideBack;
+
+  const BasicAppBar({
+    super.key,
+    this.title,
+    this.hideBack = false,
+  });
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
@@ -25,25 +30,28 @@ class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
         leadingWidth: 32,
         centerTitle: true,
         title: title,
-        leading: IconButton(
-          style: IconButton.styleFrom(
-            padding: EdgeInsets.zero,
-            fixedSize: const Size(32, 32),
-          ),
-          icon: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: context.isDark ? Color(0xFF222222) : Color(0xFFE8E8E8),
-            ),
-            child: Icon(
-              Icons.arrow_back_ios_new,
-              size: 12,
-              color: context.isDark ? AppColors.white : AppColors.black,
-            ),
-          ),
-          onPressed: onBack,
-        ),
+        leading: hideBack!
+            ? null
+            : IconButton(
+                style: IconButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  fixedSize: const Size(32, 32),
+                ),
+                icon: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color:
+                        context.isDark ? Color(0xFF222222) : Color(0xFFE8E8E8),
+                  ),
+                  child: Icon(
+                    Icons.arrow_back_ios_new,
+                    size: 12,
+                    color: context.isDark ? AppColors.white : AppColors.black,
+                  ),
+                ),
+                onPressed: onBack,
+              ),
       ),
     );
   }

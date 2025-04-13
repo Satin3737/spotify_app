78b34a642b340bca3a6277c4642e47af1980423e
satin <satin3737@gmail.com>
2025-04-13 16:26:42 +0300

    added const app urls

---
+++ lib/core/constans/app_urls.dart
@@ -0,0 +1,5 @@
+class AppUrls {
+  static const firestorage =
+      'https://firebasestorage.googleapis.com/v0/b/spotify-app-fb374.firebasestorage.app/o/covers%2F';
+  static const mediaAlt = 'alt=media';
+}

2101bbec2fbb5c2fdd25f6d3ca336c64487a264b
satin <satin3737@gmail.com>
2025-04-15 21:40:04 +0300

    Added placeholder img

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -2,6 +2,7 @@ class AppImages {
   static const String _basePath = 'assets/images/';
   static const _format = '.png';
 
+  static const String placeholder = '${_basePath}image_placeholder$_format';
   static const String introBg = '${_basePath}intro_bg$_format';
   static const String chooseModeBg = '${_basePath}choose_mode_bg$_format';
   static const String authBg = '${_basePath}auth_bg$_format';
